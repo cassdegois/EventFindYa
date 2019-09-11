5ade999920256a8e98661fded6143b30e8d617a8
Cass DeGois <cassdegois@gmail.com>
2019-09-11 16:46:45 +0800

    Event Form

--- eventFinderApp/views.py
+++ eventFinderApp/views.py
@@ -1,8 +1,8 @@
 from django.http import HttpResponse
 from django.views import generic
 from django.shortcuts import render
-from .models import Event
-from .forms import EventForm
+from .models import Event, Account
+from .forms import EventForm, AccountForm
 
 
 class IndexView(generic.ListView):
@@ -18,10 +18,36 @@ class EventView(generic.DetailView):
     model = Event
     template_name = 'eventFinderApp/event.html'
 
+class AccountView(generic.DetailView):
+    model = Account
+    template_name = 'eventFinderApp/account.html'
 
 def account(request):
-    return render(request, 'eventFinderApp/account.html')
+    accountform = AccountForm()
+    return render(request, 'eventFinderApp/account.html', {'accountform': accountform})
+
+# class AddEventCreateView(generic.CreateView):
+#     # using the create view we can just give it the variables 
+#     # as the functionaity is already built in!
+#     form_class = EventForm
+#     template_name = 'eventFinderApp/addevent.html'
+#     success_url = reverse_lazy('eventFinderApp:index')
 
 def add_event(request):
-    eventform = EventForm()
-    return render(request, 'eventFinderApp/add-event.html', {'eventform' : eventform})
+    # if this is a POST request we need to process the form data
+    if request.method == 'POST':
+        # create a form instance and populate it with data from the request:
+        form = EventForm(request.POST)
+        # check whether it's valid:
+        if form.is_valid():
+            # process the data in form.cleaned_data as required
+            # ...
+            # redirect to a new URL:
+            # return HttpResponseRedirect('/thanks/')
+            form.save()
+            return HttpResponseRedirect(reverse('eventFinderApp:index'))
+        return render(request, 'eventFinderApp/addevent.html', {'eventform': form})
+    # if a GET (or any other method) we'll create a blank form
+    else:
+        eventform = EventForm()
+        return render(request, 'eventFinderApp/addevent.html', {'eventform': eventform})
